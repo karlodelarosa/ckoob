b20b7402ff5cc22eceb637350cbb4947bdca3891
karlodlrosa <jrosa@salespond.com>
2023-04-14 00:48:33 +0800

    Added animation style

--- src/components/organism/navigation/NavMenu.vue
+++ src/components/organism/navigation/NavMenu.vue
@@ -10,7 +10,11 @@
               </div>
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                  <template  v-for="item in navigation" :key="item.name">
+                    <router-link :to="{ name: item.name }" :class="[ item.name == routeName ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']">
+                      {{ item.name }}
+                    </router-link>
+                  </template>
                 </div>
               </div>
             </div>
@@ -49,7 +53,7 @@
   
         <DisclosurePanel class="md:hidden">
           <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href">{{ item.name }}</DisclosureButton>
           </div>
           <div class="border-t border-gray-700 pt-4 pb-3">
             <div class="flex items-center px-5">
@@ -94,15 +98,16 @@
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   }
   const navigation = [
-    { name: 'Dashboard', href: '/dashboard', current: true },
+    { name: 'Dashboard', href: '/dashboard' },
     // { name: 'Reports', href: '/reports', current: false },
-    { name: 'Projects', href: '/projects', current: false },
+    { name: 'Projects', href: '/projects' },
   ]
   const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
+    // { name: 'Your Profile', href: '#' },
+    // { name: 'Settings', href: '#' },
     { name: 'Sign out', href: '/' },
   ]
 
-  const routeName = computed(() => useRoute().name)
+  const route = useRoute()
+  const routeName = computed(() => route.name)
   </script>
